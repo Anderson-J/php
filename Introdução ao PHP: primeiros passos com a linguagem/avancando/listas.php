eb915bac438b2cf3f9f7e50f4dea0b56024fe5e7
Anderson <anderaoliv@gmail.com>
2022-06-23 01:35:07 -0300

    adicionado arquivo de listas

---
+++ Introdução ao PHP: primeiros passos com a linguagem/avancando/listas.php
@@ -0,0 +1,15 @@
+<?php
+
+$idadeList1 = [21, 23, 19, 25, 30, 41, 18];
+
+$idadeList2 = array (21, 23, 19, 25, 30, 41, 18);
+
+
+$primeiraIdade = $idadeList1[0];
+
+echo $primeiraIdade;
+
+echo PHP_EOL;
+$umaIdade = $idadeList2[4];
+
+echo $umaIdade;
